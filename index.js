168ec4a27d4838a4306e2caa69bff940ea14d5b4
Luiz <luizfloresgoncalvesneto@gmail.com>
2023-09-13 02:14:53 -0300

    em andamento trabalho-final

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
 
 const arrayDoadores = []
-
-    const cadastrarDoador = () => {
-        const nome = prompt("Informe seu nome completo!")
-        const idade = (prompt("Informe sua idade!"))
-        const peso = (prompt("Informe seu peso!"))
-        const tipoSang = prompt("Informe seu tipo sanguíneo!").toLocaleUpperCase()
-        const ultimaDoacao = prompt("Informe data da última doação DD/MM/AAAA!")
-        const doador = {
-            nome: nome,
-            idade: idade,
-            peso: peso,
-            tipoSang: tipoSang,
-            ultimaDoacao: ultimaDoacao,
-        }
-        arrayDoadores.push(doador) 
-        menu()
-    }
-    const listarDoadores = () =>{
-        let lista = `
-        -----------------
-        LISTA DE DOADORES:
-        -----------------
-NOME        | IDADE  | PESO | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO
+//===================//=====================//====================//=========
+const cadastrarDoador = () => {
+const nome = prompt("Informe seu nome completo!")
+const idade = (prompt("Informe sua idade!"))
+const peso = (prompt("Informe seu peso!"))
+const tipoSang = prompt("Informe seu tipo sanguíneo!").toLocaleUpperCase()
+const ultimaDoacao = prompt("Informe data da última doação (DD/MM/AAAA):")
+const doador = {
+ nome: nome,
+ idade: idade,
+ peso: peso,
+ tipoSang: tipoSang,
+ ultimaDoacao: ultimaDoacao,
+}
+arrayDoadores.push(doador) 
+menu()
+}
+//===============//===============//===================//=================
+const listarDoadores = () =>{
+let lista = `
+-----------------------
+LISTA DE DOADORES:
+-----------------------
+NOME               | IDADE | PESO | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO
 ----------------------------------------------------------------------
 `
-        for (let i=0 ; i < arrayDoadores.length; i++){
-            const {nome, idade, peso, tipoSang, ultimaDoacao} = arrayDoadores[i]
+for (let i=0 ; i < arrayDoadores.length; i++){
+ const {nome, idade, peso, tipoSang, ultimaDoacao} = arrayDoadores[i]
 lista += 
-`${nome.padEnd(13)}|${idade.padEnd(10)}|${peso.padEnd(10)}|${tipoSang.padEnd(10)}|${ultimaDoacao.padEnd(10)}`
+`${nome.padEnd(20)}|${idade.padStart(6).padEnd(10)}|${peso.padStart(6).padEnd(9)}|${tipoSang.padStart(15).padEnd(27)}|${ultimaDoacao.padStart(15)}
+`
     
-    }
-    lista += 
-`--------------------------------------------------------------------`
-    prompt(lista)
-    menu()
 }
- 
+lista += `
+----------------------------------------------------------------------`
+prompt(lista)
+menu()
+}
+//=================//==============//================//=============
+const buscarPorTipoSangue = () => {
+const tipoSang = prompt("Digite o tipo sanguíneo desejado:").toLocaleUpperCase()
+const doador = {
+tipoSang: tipoSang,
+}
+arrayDoadores.push(doador.tipoSang)
+menu()
+}
+//===============//===================//================//============= 
+const buscarPorData = () => {
+const ultimaDoacao = prompt("Digite a data desejada (dd/mm/aaaa):")
+const doador = {
+ultimaDoacao: ultimaDoacao,
+}
+arrayDoadores.push(doador.ultimaDoacao)
+menu()
+}
+//================//==================//================//==============
 
-
-    const menu = () => {
-        const opcoes = Number(prompt(`
+const menu = () => {
+const opcoes = Number(prompt(`
 =====SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====
 1. Cadastrar doador
 2. Listar doadores
@@ -67,35 +86,7 @@ Escolha uma opção:
         default:
             alert("Opção inválida. Tente novamente.")
             menu()    
-        }   
-    }
-    menu()
-
-
-
-
-
-    
- //=====================//===================//==================//=============   
-/*3.
-const buscarPorTipoSangue = () => {
-        const tipoSanguineo = []
-    const tipo = prompt("Informe o tipo sanguíneo!")
-    alert(doador.tipoSang)
-    
-
-
-}
-//=================//====================//==================//===================
-//4.
-
-const buscarPorData = () => {
-const data = prompt("Informe a data da última doação dd/mm/aaaa")
-alert(doador.ultimaDoacao)
+        } 
+menu()          
 }
 
-
-        
-        
-
-*/
